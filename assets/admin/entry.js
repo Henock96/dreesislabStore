a9df81fc7246e21e22b99b5a48e6235d4d1085d5
Mipoka Henok <mipokahenok@yahoo.fr>
2023-04-26 15:18:09 +0000

    dreesis commit

---
+++ assets/admin/entry.js
@@ -0,0 +1,5 @@
+// In this file you can import assets like images or stylesheets
+
+import logodreesis from './images/dreesislab.png';
+import dreesis from './images/logodreesislab.png';
+import './styles/main.scss';
